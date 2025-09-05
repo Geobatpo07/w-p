382faa05aa06b34f6ab2a08495aa15134527279f
Geobatpo07 <gblaguerre@solutions.ht>
2025-09-04 20:53:27 -0400

    mise a jour pour github pages 1.0.5

--- wedding-proposal/vite.config.js
+++ wedding-proposal/vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  // ⚠️ Mets le nom exact de ton repo GitHub ici
-  base: '/wedding-proposal/',
+  base: mode === 'production'
+    ? process.env.VERCEL ? '/' : '/wedding-proposal/'
+    : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
   },
   server: {
-    port: 5173, // port par défaut Vite
-    open: true, // ouvre automatiquement dans le navigateur
-  }
-})
+    port: 5173,
+    open: true,
+  },
+}))
